d2f8833762c8ff5483d9cc2c9e38e43d021484c7
Jacob Quinn <quinn.jacobd@gmail.com>
2020-07-17 08:01:50 -0600

    Implement Resource and Client API contract

--- src/Client.jl
+++ src/Client.jl
@@ -1,5 +1,33 @@
 module Client
 
+using HTTP, JSON3
 
+const SERVER = Ref{String}()
+
+function createAlbum(name, artist, year, songs)
+    body = (; name, artist, year, songs)
+    resp = HTTP.post(string(SERVER[], "/album"), [], JSON3.write(body))
+    return JSON3.read(resp.body)
+end
+
+function getAlbum(id)
+    resp = HTTP.get(string(SERVER[], "/album/$id"))
+    return JSON3.read(resp.body)
+end
+
+function updateAlbum(album)
+    resp = HTTP.post(string(SERVER[], "/album/$(album.id)"), [], JSON3.write(album))
+    return JSON3.read(resp.body)
+end
+
+function deleteAlbum(id)
+    resp = HTTP.delete(string(SERVER[], "/album/$id"))
+    return
+end
+
+function pickAlbumToListen()
+    resp = HTTP.delete(string(SERVER[], "/"))
+    return JSON3.read(resp.body)
+end
 
 end # module
--- src/Resource.jl
+++ src/Resource.jl
@@ -1,5 +1,13 @@
 module Resource
 
+using HTTP
 
+const ROUTER = HTTP.Router()
+
+HTTP.@register(ROUTER, "POST", "/album", createAlbum)
+HTTP.@register(ROUTER, "GET", "/album/*", getAlbum)
+HTTP.@register(ROUTER, "PUT", "/album/*", updateAlbum)
+HTTP.@register(ROUTER, "DELETE", "/album/*", deleteAlbum)
+HTTP.@register(ROUTER, "GET", "/", pickAlbumToListen)
 
 end # module
