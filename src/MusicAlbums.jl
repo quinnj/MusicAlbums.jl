988d3611bc8b17de5e1a14b9d6fdfcc6b2fcb676
Jacob Quinn <quinn.jacobd@gmail.com>
2020-07-19 03:07:27 -0600

    add usings

--- src/MusicAlbums.jl
+++ src/MusicAlbums.jl
@@ -1,13 +1,18 @@
 module MusicAlbums
 
 include("Model.jl")
+using .Model
 
 include("Mapper.jl")
+using .Mapper
 
 include("Service.jl")
+using .Service
 
 include("Resource.jl")
+using .Resource
 
 include("Client.jl")
+using .Client
 
 end # module
