1a9a87875643d902e9d081435553bd23ca820f60
Jacob Quinn <quinn.jacobd@gmail.com>
2020-07-19 03:10:56 -0600

    Cleanup initial routes

--- src/Resource.jl
+++ src/Resource.jl
@@ -7,16 +7,16 @@ const ROUTER = HTTP.Router()
 createAlbum(req) = Service.createAlbum(JSON3.read(req.body))::Album
 HTTP.@register(ROUTER, "POST", "/album", createAlbum)
 
-getAlbum(req) = Service.getAlbum(parse(Int, HTTP.splitpath(req.target)[2]))::Album
+getAlbum(req) = Service.getAlbum(parse(Int, HTTP.URIs.splitpath(req.target)[2]))::Album
 HTTP.@register(ROUTER, "GET", "/album/*", getAlbum)
 
-updateAlbum(req) = Service.updateAlbum(parse(Int, HTTP.splitpath(req.target)[2]), JSON3.read(req.body))::Album
+updateAlbum(req) = Service.updateAlbum(parse(Int, HTTP.URIs.splitpath(req.target)[2]), JSON3.read(req.body, Album))::Album
 HTTP.@register(ROUTER, "PUT", "/album/*", updateAlbum)
 
-deleteAlbum(req) = Service.deleteAlbum(parse(Int, HTTP.splitpath(req.target)[2]))
+deleteAlbum(req) = Service.deleteAlbum(parse(Int, HTTP.URIs.splitpath(req.target)[2]))
 HTTP.@register(ROUTER, "DELETE", "/album/*", deleteAlbum)
 
-pickAlbumToListen() = Service.pickAlbumToListen()::Album
+pickAlbumToListen(req) = Service.pickAlbumToListen()::Album
 HTTP.@register(ROUTER, "GET", "/", pickAlbumToListen)
 
 end # module
