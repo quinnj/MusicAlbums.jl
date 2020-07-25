2f7f4101bec4793fbf7d3a8c7074a69f58cce224
Jacob Quinn <quinn.jacobd@gmail.com>
2020-07-25 01:23:38 -0600

    Set cookies=true

--- src/Client.jl
+++ src/Client.jl
@@ -7,44 +7,44 @@ const SERVER = Ref{String}("http://localhost:8080")
 
 function createUser(username, password)
     body = (; username, password=base64encode(password))
-    resp = HTTP.post(string(SERVER[], "/user"), [], JSON3.write(body))
+    resp = HTTP.post(string(SERVER[], "/user"), [], JSON3.write(body); cookies=true)
     return JSON3.read(resp.body, User)
 end
 
 function deleteUser(userId)
-    resp = HTTP.delete(string(SERVER[], "/user/$userId"))
+    resp = HTTP.delete(string(SERVER[], "/user/$userId"); cookies=true)
     return
 end
 
 function loginUser(username, password)
     body = (; username, password=base64encode(password))
-    resp = HTTP.post(string(SERVER[], "/user/login"), [], JSON3.write(body))
+    resp = HTTP.post(string(SERVER[], "/user/login"), [], JSON3.write(body); cookies=true)
     return JSON3.read(resp.body, User)
 end
 
 function createAlbum(name, artist, year, songs)
     body = (; name, artist, year, songs)
-    resp = HTTP.post(string(SERVER[], "/album"), [], JSON3.write(body))
+    resp = HTTP.post(string(SERVER[], "/album"), [], JSON3.write(body); cookies=true)
     return JSON3.read(resp.body, Album)
 end
 
 function getAlbum(id)
-    resp = HTTP.get(string(SERVER[], "/album/$id"))
+    resp = HTTP.get(string(SERVER[], "/album/$id"); cookies=true)
     return JSON3.read(resp.body, Album)
 end
 
 function updateAlbum(album)
-    resp = HTTP.put(string(SERVER[], "/album/$(album.id)"), [], JSON3.write(album))
+    resp = HTTP.put(string(SERVER[], "/album/$(album.id)"), [], JSON3.write(album); cookies=true)
     return JSON3.read(resp.body, Album)
 end
 
 function deleteAlbum(id)
-    resp = HTTP.delete(string(SERVER[], "/album/$id"))
+    resp = HTTP.delete(string(SERVER[], "/album/$id"); cookies=true)
     return
 end
 
 function pickAlbumToListen()
-    resp = HTTP.get(string(SERVER[], "/"))
+    resp = HTTP.get(string(SERVER[], "/"); cookies=true)
     return JSON3.read(resp.body, Album)
 end
 
